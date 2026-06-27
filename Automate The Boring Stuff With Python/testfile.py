a7dfddb832c883a689fe14a3b044645ebc6c6a36
Daniel Wan <cwan23333@gmail.com>
2026-06-27 15:12:21 +0800

    vault backup: 2026-06-27 15:12:21

--- Automate The Boring Stuff With Python/testfile.py
+++
@@ -1,8 +0,0 @@
-while True:
-    print('Who are you?')
-    if (name := input('>')) != 'Joe':
-        continue 
-    print("Hello Joe, what's your password?")
-    if (password := input('>')) != 'swordfish':
-        break
-print('Acess granted') 
