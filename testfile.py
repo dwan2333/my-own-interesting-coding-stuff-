2089a82b12ccf9efd5df81abeb375fefac8097a2
Daniel Wan <cwan23333@gmail.com>
2026-06-28 21:02:14 +0800

    vault backup: 2026-06-28 21:02:14

--- testfile.py
+++ testfile.py
@@ -1,4 +1,13 @@
 import random 
 
-x = random.randint()
-print(x)
+guess = int(input("Guess a number between 0 to 20: "))
+
+while (x := random.randint(0,21)) != guess:
+    if guess < x:
+        print("Your guess is smaller than the mysterious number")
+    elif guess > x:
+        print("Your guess is larger than the mysterious number")
+    guess = int(input("Guess a number between 0 to 20: "))
+    
+print("You guessed the right number")
+    
