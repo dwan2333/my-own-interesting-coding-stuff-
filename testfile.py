2281328ed18e887c0b9b7f3d47a400132a05e2d7
Daniel Wan <cwan23333@gmail.com>
2026-06-28 20:54:03 +0800

    vault backup: 2026-06-28 20:54:03

--- testfile.py
+++ testfile.py
@@ -1,8 +1,4 @@
-# command = input("Enter command (type 'exit' to quit): ")
-# while command != 'exit':
-#     print(f"Executing system command: {command}")
-#     command = input("Enter command (type 'exit' to quit): ") 
+import random 
 
-while (command := input("Entrer comman (type 'exit' to quit): ")) != "exti":
-    print(f'Execitomh system commmand: {command}')
-    command = input("Enter command (type 'exit' to quit):  ")
+x = random.randint()
+print(x)
