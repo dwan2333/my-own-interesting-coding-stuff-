1310269fd0776f5a66254df2dfcaf0db12058d38
Daniel Wan <cwan23333@gmail.com>
2026-07-01 14:41:20 +0800

    vault backup: 2026-07-01 14:41:20

--- testfile2.py
+++ testfile2.py
@@ -1,10 +1,15 @@
-spam = {'Daniel' : '2004'}
-print(spam['Nathan'])
+inventory = {'rope': 1, 'torch': 6, 'gold coin': 42, 'dagger': 1, 'arrow': 12}
+
+def display_inventory(ivt):
+    print('Inventory:')
+    count = 0
+    for k,v in ivt.items():
+        print(v, k)
+        count += v
+    print(f"Total number of items : {count}")
 
 
-
-
-
+display_inventory(inventory)
 
     
 
