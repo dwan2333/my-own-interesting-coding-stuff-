7e04de220490a4f4aff8b6dcf7c5adab40860421
Daniel Wan <cwan23333@gmail.com>
2026-07-01 21:35:10 +0800

    vault backup: 2026-07-01 21:35:09

--- testfile.py
+++ testfile.py
@@ -1,6 +1,8 @@
-word = 'DANIEL'
-print(word.isupper())
+import re 
+number = re.compile(r'\d{3}-|\.|\s\d{3}')
+text = '858 308 2978, 858 297'
 
+print(number.findall(text))
     
 
 
--- testfile2.py
+++ testfile2.py
@@ -3,16 +3,16 @@ import pyperclip, re
 text = pyperclip.paste() 
 
 phone_number_match = re.compile(r''' # optional country code 
-                                (\d{3}) # leading three number 
-                                (-|\.|\b)  # seperator 
-                                (\d{3}) # body number
-                                (-|\.|\b) # seperator 
-                                (\d{4})
-                                    ''', re.VERBOSE)
+                                \d{3} # leading three number 
+                                -|\.|\s  # seperator 
+                                \d{3} # body number
+                                -|\.|\s # seperator 
+                                \d{4} # tailing numbe 
+                                ''', re.VERBOSE)
 
 text = "858-308-2978 , +1 858 308 2978, 858.308.2978"
 
-print(phone_number_match.findallmatch(text))
+print(phone_number_match.findall(text))
 
 
 
