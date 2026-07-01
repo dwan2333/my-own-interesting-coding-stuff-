5f5e8e41894eaebebbc17cb665a7c0888cadd615
Daniel Wan <cwan23333@gmail.com>
2026-07-01 15:22:27 +0800

    vault backup: 2026-07-01 15:22:27

--- testfile2.py
+++ testfile2.py
@@ -1,32 +1,16 @@
-inventory = {'rope': 1, 'torch': 6, 'gold coin': 42, 'dagger': 1, 'arrow': 12}
-dragon_loot = ['gold coin', 'dagger', 'gold coin', 'gold coin', 'ruby']
-import logging
+import pyperclip 
 
-logging.basicConfig(level = logging.DEBUG, format = "%(asctime)s - %(levelname)s - %(message)s", force = True)
+def add_bullet_point():
+    text = pyperclip.paste().split('\n')
 
-breakpoint()
-def display_inventory(ivt):
-    logging.debug("The inventory %s is being processed", ivt)
-    print('Inventory:')
-    count = 0
-    for k,v in ivt.items():
-        print(v, k)
-        count += v
-        logging.debug(f'{v} items of {k} is being counted and the total count is {count} ')
-    print(f"Total number of items : {count}")
+    for i in range(len(text)):
+        text[i] = "- " + text[i]
 
+    new_text = "\n".join(text)
+    pyperclip.copy(new_text)
 
-def add_to_inventory(ivt, loot):
-    for i in loot:
-        if i in ivt:
-            ivt[i] += 1
-        else:
-            ivt.setdefault(i,1)
-    return ivt
+add_bullet_point()
 
-new_invt = add_to_inventory(inventory, dragon_loot)
-print(new_invt)
-display_inventory(new_invt)
-
-    
+"""Okay that is fine
+I am okay """
 
