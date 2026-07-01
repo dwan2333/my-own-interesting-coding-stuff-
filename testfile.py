359f0793873eeb1e539403dbd9d920b27e2e846c
Daniel Wan <cwan23333@gmail.com>
2026-07-01 21:43:23 +0800

    vault backup: 2026-07-01 21:43:22

--- testfile.py
+++ testfile.py
@@ -1,8 +1,5 @@
-import re 
-number = re.compile(r'\d{3}-|\.|\s\d{3}')
-text = '858 308 2978, 858 297'
-
-print(number.findall(text))
+import re
+print(re.findall(r'(?:\d{3})-(\d{4})', '555-1234')) 
     
 
 
--- testfile2.py
+++ testfile2.py
@@ -2,12 +2,12 @@ import pyperclip, re
 
 text = pyperclip.paste() 
 
-phone_number_match = re.compile(r''' # optional country code 
+phone_number_match = re.compile(r'''(?:\+?\d[-\.\s])? # optional country code 
                                 \d{3} # leading three number 
-                                -|\.|\s  # seperator 
+                                [-\.\s]  # seperator 
                                 \d{3} # body number
-                                -|\.|\s # seperator 
-                                \d{4} # tailing numbe 
+                                [-\.\s] # seperator 
+                                \d{4} # tailing number
                                 ''', re.VERBOSE)
 
 text = "858-308-2978 , +1 858 308 2978, 858.308.2978"
