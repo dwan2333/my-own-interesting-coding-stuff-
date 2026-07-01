16bbc03c6f05988229c4457f1104025e5d9cf1b5
Daniel Wan <cwan23333@gmail.com>
2026-07-01 16:11:36 +0800

    vault backup: 2026-07-01 16:11:36

--- testfile2.py
+++ testfile2.py
@@ -1,12 +1,17 @@
 print("Please enter the sentence that you want to translate:")
-text = str(input(">").split())
+text = input(">").split()
 import logging
 
 logging.basicConfig(level = logging.DEBUG, format = '%(asctime)s - %(levelname)s - %(message)s', force = True)
-logging.debug('Test')
-for word in range(len(text)):
-    
-    if not word.isalpha(): 
-        for character in word:
-            if not character.isaplpha():
-                print()
+for i in range(len(text)):
+    logging.debug("word %s is being tested for isalpha", text[i])
+    if not text[i].isalpha(): 
+        logging.debug("word %s did not pass the test", text[i])
+        left_stripped_character = ""
+        for character in text[i]:
+            logging.debug("character %s is being tested for isalpha", character)
+            if not character.isalpha():
+                left_stripped_character += text[i].lstrip(character)
+                logging.debug("%s is being stripped out from the left side and added to the left striped characters",(character))
+
+                
