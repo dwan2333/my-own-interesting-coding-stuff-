bb8dba06427f574493400b241d6a2ff8f50656bc
Daniel Wan <cwan23333@gmail.com>
2026-07-06 21:58:20 +0800

    vault backup: 2026-07-06 21:58:20

--- testfile.py
+++ testfile.py
@@ -1,13 +1,30 @@
+import random, logging
 from pathlib import Path
-import os
-P = Path("D:/testfile.txt")
 
-print(P.stat().st_mtime)
-    
-
+capitals = {'Alabama': 'Montgomery', 'Alaska': 'Juneau', 'Arizona':
+'Phoenix', 'Arkansas': 'Little Rock', 'California': 'Sacramento', 'Colorado':
+'Denver', 'Connecticut': 'Hartford', 'Delaware': 'Dover', 'Florida':
+'Tallahassee', 'Georgia': 'Atlanta', 'Hawaii': 'Honolulu', 'Idaho': 'Boise',
+'Illinois': 'Springfield', 'Indiana': 'Indianapolis', 'Iowa': 'Des Moines',
+'Kansas': 'Topeka', 'Kentucky': 'Frankfort', 'Louisiana': 'Baton Rouge',
+'Maine': 'Augusta', 'Maryland': 'Annapolis', 'Massachusetts': 'Boston',
+'Michigan': 'Lansing', 'Minnesota': 'Saint Paul', 'Mississippi': 'Jackson',
+'Missouri': 'Jefferson City', 'Montana': 'Helena', 'Nebraska': 'Lincoln',
+'Nevada': 'Carson City', 'New Hampshire': 'Concord', 'New Jersey': 'Trenton',
+'New Mexico': 'Santa Fe', 'New York': 'Albany', 'North Carolina': 'Raleigh',
+'North Dakota': 'Bismarck', 'Ohio': 'Columbus', 'Oklahoma': 'Oklahoma City',
+'Oregon': 'Salem', 'Pennsylvania': 'Harrisburg', 'Rhode Island': 'Providence',
+'South Carolina': 'Columbia', 'South Dakota': 'Pierre', 'Tennessee':
+'Nashville', 'Texas': 'Austin', 'Utah': 'Salt Lake City', 'Vermont':
+'Montpelier', 'Virginia': 'Richmond', 'Washington': 'Olympia', 
+'West Virginia':'Charleston', 'Wisconsin': 'Madison', 'Wyoming': 'Cheyenne'}
 
         
-        
+logging.basicConfig(level = logging.DEBUG, format = "%(acstime)s - %(levelname)s - %(message)s")  
+
+print(Path.cwd())
+
+
 
 
     
