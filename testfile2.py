7707163f77e1606c56ee06b7f1252c835e0bf9aa
Daniel Wan <cwan23333@gmail.com>
2026-07-07 15:23:48 +0800

    vault backup: 2026-07-07 15:23:47

--- testfile2.py
+++ testfile2.py
@@ -1,35 +1,7 @@
 import random
-capitals = {'Alabama': 'Montgomery', 'Alaska': 'Juneau', 'Arizona':
-'Phoenix', 'Arkansas': 'Little Rock', 'California': 'Sacramento', 'Colorado':
-'Denver', 'Connecticut': 'Hartford', 'Delaware': 'Dover', 'Florida':
-'Tallahassee', 'Georgia': 'Atlanta', 'Hawaii': 'Honolulu', 'Idaho': 'Boise',
-'Illinois': 'Springfield', 'Indiana': 'Indianapolis', 'Iowa': 'Des Moines',
-'Kansas': 'Topeka', 'Kentucky': 'Frankfort', 'Louisiana': 'Baton Rouge',
-'Maine': 'Augusta', 'Maryland': 'Annapolis', 'Massachusetts': 'Boston',
-'Michigan': 'Lansing', 'Minnesota': 'Saint Paul', 'Mississippi': 'Jackson',
-'Missouri': 'Jefferson City', 'Montana': 'Helena', 'Nebraska': 'Lincoln',
-'Nevada': 'Carson City', 'New Hampshire': 'Concord', 'New Jersey': 'Trenton',
-'New Mexico': 'Santa Fe', 'New York': 'Albany', 'North Carolina': 'Raleigh',
-'North Dakota': 'Bismarck', 'Ohio': 'Columbus', 'Oklahoma': 'Oklahoma City',
-'Oregon': 'Salem', 'Pennsylvania': 'Harrisburg', 'Rhode Island': 'Providence',
-'South Carolina': 'Columbia', 'South Dakota': 'Pierre', 'Tennessee':
-'Nashville', 'Texas': 'Austin', 'Utah': 'Salt Lake City', 'Vermont':
-'Montpelier', 'Virginia': 'Richmond', 'Washington': 'Olympia', 
-'West Virginia':'Charleston', 'Wisconsin': 'Madison', 'Wyoming': 'Cheyenne'}
-
-# convert the dict into list would be eazier to apply to iterables
-def dict_shuffle(dict):
-    output = []
-    for value, key in dict.items():
-        (inner_layer := [value,key])
-        output.append(inner_layer)
-    return output
-
-x = dict_shuffle(capitals)
-
-for n in range(len(x)):
-    for s in x[n]:
-        print(s)
+x=[1,2,3,4]
+y = random.sample(x, k=4)
+print(y)
 
 
 
