5de84308668ede277affb94c095b6c50709b173b
Daniel Wan <cwan23333@gmail.com>
2026-07-07 16:29:16 +0800

    vault backup: 2026-07-07 16:29:16

--- testfile2.py
+++ testfile2.py
@@ -1,31 +1,10 @@
-States_and_Captials = {'Alabama': 'Montgomery', 'Alaska': 'Juneau', 'Arizona':
-'Phoenix', 'Arkansas': 'Little Rock', 'California': 'Sacramento', 'Colorado':
-'Denver', 'Connecticut': 'Hartford', 'Delaware': 'Dover', 'Florida':
-'Tallahassee', 'Georgia': 'Atlanta', 'Hawaii': 'Honolulu', 'Idaho': 'Boise',
-'Illinois': 'Springfield', 'Indiana': 'Indianapolis', 'Iowa': 'Des Moines',
-'Kansas': 'Topeka', 'Kentucky': 'Frankfort', 'Louisiana': 'Baton Rouge',
-'Maine': 'Augusta', 'Maryland': 'Annapolis', 'Massachusetts': 'Boston',
-'Michigan': 'Lansing', 'Minnesota': 'Saint Paul', 'Mississippi': 'Jackson',
-'Missouri': 'Jefferson City', 'Montana': 'Helena', 'Nebraska': 'Lincoln',
-'Nevada': 'Carson City', 'New Hampshire': 'Concord', 'New Jersey': 'Trenton',
-'New Mexico': 'Santa Fe', 'New York': 'Albany', 'North Carolina': 'Raleigh',
-'North Dakota': 'Bismarck', 'Ohio': 'Columbus', 'Oklahoma': 'Oklahoma City',
-'Oregon': 'Salem', 'Pennsylvania': 'Harrisburg', 'Rhode Island': 'Providence',
-'South Carolina': 'Columbia', 'South Dakota': 'Pierre', 'Tennessee':
-'Nashville', 'Texas': 'Austin', 'Utah': 'Salt Lake City', 'Vermont':
-'Montpelier', 'Virginia': 'Richmond', 'Washington': 'Olympia', 
-'West Virginia':'Charleston', 'Wisconsin': 'Madison', 'Wyoming': 'Cheyenne'}
+import sys
+import subprocess
 
-States = [keys for keys, values in States_and_Captials.items()]
+# This forces the specific Python environment your editor is using right now to install yfinance
+print("Current editor python path:", sys.executable)
+subprocess.check_call([sys.executable, '-m', 'pip', 'install', 'yfinance'])
 
-print(States_and_Captials.get(States[0]))
-
-
-
-
-
-
-
-
-
-                
+# Now try importing it
+import yfinance as yf
+print("Successfully imported yfinance version:", yf.__version__)
