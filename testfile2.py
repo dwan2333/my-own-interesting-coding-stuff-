0ca790139951b388e8ee9fad90871b025b5a409d
Daniel Wan <cwan23333@gmail.com>
2026-07-08 14:55:39 +0800

    vault backup: 2026-07-08 14:55:39

--- testfile2.py
+++ testfile2.py
@@ -5,6 +5,12 @@ from datetime import date, time, datetime, timedelta
 dt = datetime.strptime("July 7 2026", "%B %d %Y")
 
 end_dt = datetime.strptime("December 25 2026", "%B %d %Y")
+
+print(dt)
 print(f"Start date: {dt.date()}")
 print(f"End date: {end_dt.date()}")
 print(f"Days between: {(end_dt - dt).days} days")
+
+
+dt = date.strptime('January 1 2000', "%B %A %Y")
+print(dt.weekday())
