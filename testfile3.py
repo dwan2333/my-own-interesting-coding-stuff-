ff0d14841fb3dca572fa617f06841d38c481e96a
Daniel Wan <cwan23333@gmail.com>
2026-07-08 23:28:15 +0800

    vault backup: 2026-07-08 23:28:15

--- testfile3.py
+++ testfile3.py
@@ -1,6 +1,13 @@
-from pathlib import Path
+import os
+os.environ['http_proxy'] = ''
+os.environ['https_proxy'] = ''
+os.environ['HTTP_PROXY'] = ''
+os.environ['HTTPS_PROXY'] = ''
+os.environ['ALL_PROXY'] = ''
 
-location = Path(input(">>> "))
+import yfinance as yf
 
-location.mkdir(exist_ok=True, parents=True)
+data = yf.download('SPCX', period = 'max', interval = '1d' )
+
+data.to_csv('Spacex_Historical_Chart.csv')
 
