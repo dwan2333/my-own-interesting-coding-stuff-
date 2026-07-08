f7b0c3f5621f15d59eae0583295d5012931e17e2
Daniel Wan <cwan23333@gmail.com>
2026-07-08 17:46:08 +0800

    vault backup: 2026-07-08 17:46:08

--- testfile2.py
+++ testfile2.py
@@ -1,4 +1,7 @@
-from datetime import datetime
-d = datetime.strptime('2026-12-25', '%Y-%m-%d')
-print(d.strftime('%B %d, %Y'))
-print(d)
+# Problem. Starting from 2026-07-07 09:00:00, what date and time is it 90 days and 12 hours later?
+from datetime import date, datetime
+
+def is_leapyear(year):
+    time = date(year,2,29)
+
+time = date(int(input('select a year')),2,29)   
