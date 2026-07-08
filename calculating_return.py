6aa25763a10532cf89b20d820719f37a0ab4691d
Daniel Wan <cwan23333@gmail.com>
2026-07-09 00:00:53 +0800

    vault backup: 2026-07-09 00:00:53

--- calculating_return.py
+++ calculating_return.py
@@ -1,3 +1,5 @@
+# making sure that the system know that internet is not though a proxy
+from msilib.schema import Error
 import os
 os.environ['http_proxy'] = ''
 os.environ['https_proxy'] = ''
@@ -5,11 +7,23 @@ os.environ['HTTP_PROXY'] = ''
 os.environ['HTTPS_PROXY'] = ''
 os.environ['ALL_PROXY'] = ''
 
+# fetch the historical chart from yahoo in csv
 import yfinance as yf
 
+import csv
 
-data = yf.download('SPCX')
-data.to_csv('spacex_historical_chart.csv')
+# download the historical chart for your choice of stock
+ticker = 'SPCX'
+try:
+    data = yf.download(ticker)
+    data.to_csv(f'{ticker}_historical_chart.csv')
+except Error:
+    pass
+
+open(f'{ticker}_historical_chart.csv')
+
+
+
 
 
 
