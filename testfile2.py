2a4759f8caa5785c8a18ab94a5fa11aae377292d
Daniel Wan <cwan23333@gmail.com>
2026-07-08 18:26:52 +0800

    vault backup: 2026-07-08 18:26:52

--- testfile2.py
+++ testfile2.py
@@ -1,12 +1,4 @@
-# Problem. Starting from 2026-07-07 09:00:00, what date and time is it 90 days and 12 hours later?
-from datetime import date, datetime
+def average(*num):
+    return(sum(num)/len(num))
 
-
-def is_leapyear(year):
-    try:
-        time = date(year,2,29)
-        return (f'{time} is a leap year')
-    except ValueError:
-        return (f'{year} is not a leap year')
-
-print(is_leapyear(2004))
+print(average(2,2,2))
