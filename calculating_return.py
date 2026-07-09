ada9d8d22979c043bdc69f9bd5624c9c02d23975
Daniel Wan <cwan23333@gmail.com>
2026-07-09 16:22:36 +0800

    vault backup: 2026-07-09 16:22:36

--- calculating_return.py
+++ calculating_return.py
@@ -9,16 +9,24 @@ os.environ['ALL_PROXY'] = ''
 import yfinance as yf
 
 # download the historical chart for your choice of stock
+from pathlib import Path
 ticker = input("Please choose the ticker for the stock: ")
 data = yf.download(ticker, interval = '1d', period = 'max')
 file_name = f'{ticker}_historical_chart.csv'
-data.to_csv(file_name)
+if Path(Path.cwd()/f'{file_name}').exists():
+    pass
+else:
+    data.to_csv(file_name)
 
 import csv
 
 target_file = open(file_name)
 to_read_target = csv.reader(target_file)
 
+
+for row in to_read_target:
+    open = row[4]
+    print(open)
 
 
 
